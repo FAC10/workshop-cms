fbdc40cdc33770bf7b858fcf53ae93f25c48a8ec
Yvonne Liu <liuyvonne.ca@gmail.com>
2017-03-13 15:39:26 +0000

    coplte step 5 and add extensions to handler

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ function handler (request, response){
   var method = request.method;
 
   var endpoint = request.url;
+  console.log(endpoint);
 
   if (endpoint === "/"){
     response.writeHead(200, {'Content-Type': 'text/html'});
@@ -15,10 +16,33 @@ function handler (request, response){
         console.log(err);
         return;
       }
-
       response.end(file);
     })
   }
+
+  else {
+    var extension = endpoint.split('.')[1];
+    var extensionType = {
+      'html': 'text/html',
+      'css': 'text/css',
+      'js': 'application/javascript',
+      'ico': 'image/x-icon',
+      'jpg': 'image/jpg',
+      'png': 'image/png'
+      }
+    response.writeHead(200, {'Content-Type': extensionType[extension]});
+    console.log(__dirname + '/../public'+ endpoint);
+    fs.readFile(__dirname + '/../public'+ endpoint, function(err, file){
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("I'm running")
+      response.end(file);
+    });
+  }
+
+
 }
 
 var server = http.createServer(handler);
