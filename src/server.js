abaaa00fcd5d127f317ba5cc73ec1e57c63b170a
Zooey <zooey@lbk.org.uk>
2017-03-13 14:56:47 +0000

    add handler function logic

--- src/server.js
+++ src/server.js
@@ -1,11 +1,24 @@
 var http = require("http");
+var fs = require("fs");
 
 var message = 'I am watching you';
 
 function handler (request, response){
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write(message);
-  response.end();
+  var method = request.method;
+
+  var endpoint = request.url;
+
+  if (endpoint === "/"){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile(__dirname + '/../public/index.html', function(err, file){
+      if (err){
+        console.log(err);
+        return;
+      }
+
+      response.end(file);
+    })
+  }
 }
 
 var server = http.createServer(handler);
